826311c489d1a594f50f88498cad159c3a5ead11
Paraschiv Daniel <49078467+ParaschivDaniel@users.noreply.github.com>
2020-02-02 18:00:53 +0200

    Updates

---
+++ precache-manifest.68339f43f0f935628983e9f8e460b3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29d4d102e5879db4f37aa675d866e80",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "efd533c6e6d7f260c140",
+    "url": "/MonsterRolodex/static/css/main.8fc906f8.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/MonsterRolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterRolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efd533c6e6d7f260c140",
+    "url": "/MonsterRolodex/static/js/main.4aefc7d8.chunk.js"
+  },
+  {
+    "revision": "c22fd1833dfaa53579b2",
+    "url": "/MonsterRolodex/static/js/runtime-main.d5acbf4c.js"
+  }
+]);
